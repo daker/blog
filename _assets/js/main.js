e1ed0c5d72b356f94bf0f3bec1128d5e5ff70c84
Adnane Belmadiaf <adnane002@gmail.com>
2013-05-26 15:30:55 +0100

    Removed the readingtime

---
+++ _assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    var text = $('.post-field').text(),
+        words = text.split(' ').length,
+        wpm = 200,
+        time = (words / wpm) * 60,
+        minutes = Math.floor(time / 60),
+        seconds = Math.floor(time - minutes * 60),
+        content = '';
+
+    if (seconds > 0) {
+        content = '<span title="'+ words +' word">' + minutes + ' mins and ' + seconds + ' secs</span>';
+    }
+    else {
+        content = '<span title="'+ words +' word">' + minutes+ '</span>';;
+    }
+    $('#readingtime').html(content);
+
+    $(".post-field img").each(function() {
+        if($(this).attr("alt"))
+            $(this).wrap('<figure class="post-image"></figure>')
+                .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+    });
+})
