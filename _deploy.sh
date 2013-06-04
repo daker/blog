f221397379ccf7cc975b268c0bd127b6ae93e4ff
Adnane Belmadiaf <adnane002@gmail.com>
2013-06-04 12:18:38 +0100

    Fixed deploy script

--- _deploy.sh
+++ _deploy.sh
@@ -1,2 +1,4 @@
-mynt gen -f _site && git add . && git commit
-cp -r _site ../daker.github.io/
+mynt gen -f _site && git add . && git commit && git push
+cp -r _site/* ../daker.github.io/
+cd ../daker.github.io/
+git add . && git commit  && git push
