2a00ce57ac091e4fa2cf14ea1bdf52dad19ec348
Adnane Belmadiaf <adnane002@gmail.com>
2013-09-19 21:08:18 +0100

    Added fusionAd code

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,12 +1,6 @@
 $(document).ready(function () {
 
-    var text = $('.post-field').text(),
-        words = text.split(' ').length,
-        wpm = 200,
-        time = (words / wpm) * 60,
-        minutes = Math.floor(time / 60),
-        seconds = Math.floor(time - minutes * 60),
-        content,
+    var content,
         posts,
         monthNames = ["January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"
@@ -21,13 +15,6 @@ $(document).ready(function () {
         year,
         date,
         wrapper;
-
-    if (seconds > 0) {
-        content = '<span title="' + words + ' words">' + minutes + ' mins and ' + seconds + ' secs</span>';
-    } else {
-        content = '<span title="' + words + ' words">' + minutes + ' minute</span>';
-    }
-    $('#readingtime').html(content);
 
     $(".post-field img").each(function () {
         if ($(this).attr("alt"))
