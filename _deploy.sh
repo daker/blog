0b1b910d207dc2881aeb9349245f65f9f02e668a
Adnane Belmadiaf <adnane002@gmail.com>
2014-01-11 18:08:34 +0000

    Refactoring

--- _deploy.sh
+++ _deploy.sh
@@ -1,6 +1,6 @@
 grunt
 mynt gen -f _site && git add . && git commit && git push
-cp -r demos/* _site/demos/
-cp -r _site/* ../daker.github.io/
+rsync -rv demos/* _site/demos/
+rsync -rv --exclude=*.styl --exclude=node_modules _site/* ../daker.github.io/
 cd ../daker.github.io/
 git add . && git commit  && git push
